0c3f6e576e07c9c3d14e326f675f3b44d50c7b55
Zohaib <zohaib.hassan@mail.mcgill.ca>
2018-07-14 02:58:05 +0000

    Refactored code

--- chatty_server/server.js
+++ chatty_server/server.js
@@ -1,89 +1,72 @@
-// server.js
-
 const express = require('express');
 const SocketServer = require('ws').Server;
 const uuidv4 = require('uuid/v4')
 
-// Set the port to 3001
 const PORT = 3001;
 
-// Create a new express server
 const server = express()
-   // Make the express server serve static assets (html, javascript, css) from the /public folder
   .use(express.static('public'))
   .listen(PORT, '0.0.0.0', 'localhost', () => console.log(`Listening on ${ PORT }`));
 
-// Create the WebSockets server
 const wss = new SocketServer({ server });
 
+
 let updateClientCount = () => {
+
   let connectObj = {};
   connectObj.type = "connection";
   connectObj['count'] = wss.clients.size;
   connectObj.id = uuidv4();
   
- return cnxObj;
+  return connectObj;
 };
-console.log(updateClientCount())
 
-// Set up a callback that will run when a client connects to the server
-// When a client connects they are assigned a socket, represented by
-// the ws parameter in the callback.
+
 wss.on('connection', function connection(ws) {
   ws.on('message', function incoming(message) {
-  	let message1 = JSON.parse(message)
-  	switch(message1.type){
+
+  	let parsedMessage = JSON.parse(message);
+  	switch(parsedMessage.type){
   		case "postMessage":
-  		message1['id'] = uuidv4();
-  		message1['type'] = 'incomingMessage';
-  		wss.clients.forEach(function each(client) {
-  		   
-  		    	var toSend = JSON.stringify(message1)
-  		      client.send(toSend);
-  		    
+    		parsedMessage['id'] = uuidv4();
+    		parsedMessage['type'] = 'incomingMessage';
+
+    		wss.clients.forEach(function each(client) {   		   
+		    	let backToClient = JSON.stringify(parsedMessage);
+		      client.send(backToClient);    
   		  });
-  		break;
+  		  break;
 
   		case "postNotification":
-  		message1['type'] = 'incomingNotification';
-  		message1['id'] = uuidv4()
-  		wss.clients.forEach(function each(client) {
-  		   
-  		    	var toSend = JSON.stringify(message1)
-  		      client.send(toSend);
-  		    
+    		parsedMessage['type'] = 'incomingNotification';
+    		parsedMessage['id'] = uuidv4();
+
+    		wss.clients.forEach(function each(client) { 
+		    	let backToClient = JSON.stringify(parsedMessage);
+		      client.send(backToClient);
   		  });
-  		break;
-
+  		  break;
   	}
-
-
-   
   });
 
-  wss.clients.forEach(function each(client) {
-    var countFunction = updateClientCount();
-    var updateCount = JSON.stringify(countFunction);
 
-     client.send(updateCount);
-     
-    });
+  wss.clients.forEach(function each(client) {
+    let updateUserCount = updateClientCount();
+    let stringUpdateUserCount = JSON.stringify(updateUserCount);
+
+    client.send(stringUpdateUserCount);
+
+  });
 
    ws.on('close', () => {
+
     wss.clients.forEach(function each(client) {
-      var countFunction = updateClientCount();
-      var updateCount = JSON.stringify(countFunction);
+     let updateUserCount = updateClientCount();
+     let stringUpdateUserCount = JSON.stringify(updateUserCount);
 
-       client.send(updateCount);
+     client.send(stringUpdateUserCount);
        
-      });
-
-  
-      
     });
-    
 
-
-
- 
-});
+  });
+ });
--- src/App.jsx
+++ src/App.jsx
@@ -2,19 +2,18 @@ import React, {Component} from 'react';
 import Chatbar from './Chatbar.jsx'
 import MessageList from './MessageList.jsx'
 
-
-
 class App extends Component {
 
   constructor(props) {
+
     super(props);
 
     this.state = {
+
       currentUser: {name: "Bob"}, 
       messages: [], 
       users:{userNum: 0}
     }
-
     this.socket = new WebSocket('ws://localhost:3001');
   }
 
@@ -22,100 +21,91 @@ class App extends Component {
   componentDidMount() {
  
     this.socket.onmessage =  (event) => {
-      console.log(event.data);
+     
       const incomingMessage = JSON.parse(event.data);
+
       switch(incomingMessage.type){
+
       case 'incomingMessage':  
-      this.updateMessage(incomingMessage)
-      break;
+        this.updateMessages(incomingMessage)
+        break;
+
       case 'incomingNotification':
-      this.updateUser(incomingMessage)
-      break;
+        this.updateUsers(incomingMessage)
+        break;
+
       case 'connection':
-      console.log('get here')
-      this.setState( {users: {userNum: incomingMessage.count}} );
-      break;
+        this.setState( {users: {userNum: incomingMessage.count}} );
+        break;
 
-    }
-    }
+      default:
+        throw new Error("Unknown event type " + incomingMessage.type);
 
-    
-         
+      }
+    }  
+  }
+ 
 
-         }
+  updateMessages = (incomingMessage) => {
 
-  updateMessage = (incomingMessage)    => {
-    console.log(this.state.messages, 'got here')
-      this.setState({  messages: [
-        ...this.state.messages,
+    this.setState({ messages: [
+      ...this.state.messages,
       { 
         type: incomingMessage.type,
         id: incomingMessage.id,
         username: incomingMessage.username,
         content: incomingMessage.content
-      }
-      
-    ]
-
-      })
-
+      } ]
+    })
   }
 
-  updateUser = (incomingMessage)    => {
-    console.log(this.state.messages, 'fdgfgf')
-      this.setState({  messages: [
-        ...this.state.messages,
+
+  updateUsers = (incomingMessage) => {
+
+    this.setState({  messages: [
+      ...this.state.messages,
       { 
         type: incomingMessage.type,
         id: incomingMessage.id,
         username: '',
         content: incomingMessage.content
-      }
-      
-    ]
-
-      })
-
+      } ]
+    })
   }
 
 
+  addUser = (newUser) => {
 
-  newUser = (newUser) =>{
     let userChange = {
-      type: "postNotification", content: this.state.currentUser.name + "changed their name to  " + newUser 
+      type: "postNotification", content: `${this.state.currentUser.name} changed their name to ${newUser}` 
     }
-    this.setState({currentUser: {name : newUser}})
-    const ws = this.socket 
-    this.socket.send(JSON.stringify(userChange))
 
-   
+    this.setState({currentUser: {name : newUser}});
+
+    this.socket.send(JSON.stringify(userChange)); 
   }    
 
-  addingMessage = (message) => {
-    //Do the setState stuff in here
-    //this.setState({
-    
+  addMessage = (message) => {
 
-     let messages = {
-        type: 'postMessage',
-        username: this.state.currentUser.name,
-        content: message  
-      }
-      const ws = this.socket 
-      this.socket.send(JSON.stringify(messages))
-     
-    
+    let messages = {
+      type: 'postMessage',
+      username: this.state.currentUser.name,
+      content: message  
     }
+     
+    this.socket.send(JSON.stringify(messages))
+        
+  }
 
   render() {
     return (
     <div>
       <nav className="navbar">
         <a href="/" className="navbar-brand">Chatty</a>
-        <span className='nav-users'>Users online: {this.state.users.userNum}</span>
+        <span className='nav-users'>Online Users: {this.state.users.userNum}</span>
       </nav>
       <MessageList messages = {this.state.messages}/>
-      <Chatbar addingMessage={this.addingMessage} newUser={this.newUser} name = {this.state.currentUser.name}/>
+      <Chatbar addingMessage={this.addMessage} newUser={this.addUser} name = {this.state.currentUser.name}/>
       </div>
 
     );
